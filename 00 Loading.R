fb5abe58ea0a5388ba198b6fb3f8c6802c0dad0e
kai.mertens@tuhh.de <kai.mertens@tuhh.de>
2020-01-02 09:00:03 +0100

    update

---
+++ 00 Loading.R
@@ -0,0 +1,34 @@
+#####################################
+#### CostSystemDesignSim   // 2019-08-28    V 0.01
+#####################################
+
+
+###########
+#### Test
+###########
+
+##############################
+# 0 - Install librairies - Library
+##############################
+
+# install.packages(c(
+#   "dplyr",
+#   "tidyr",
+#   "rmarkdown",
+#   "ggplot2"
+#     
+#   
+# ))
+# 
+# Packages <- c("dplyr", "ggplot2", "rmarkdown", "tidyr")
+# lapply(Packages, library, character.only = TRUE)
+
+##############################
+# 1 - Start
+##############################
+
+## SOURCE THIS FILE FOR EXECUTION
+
+
+
+source("01 INIT.R")
